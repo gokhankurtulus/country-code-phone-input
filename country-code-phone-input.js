e64705463149dbaee20a7ffaa6a9fc9ddcd6ebd7
Gökhan Kurtuluş <gokhankurtulus@icloud.com>
2022-09-28 14:13:59 +0300

    Removed phone formatter

--- country-code-phone-input.js
+++ country-code-phone-input.js
@@ -33,8 +33,9 @@ function phoneInput(input_id) {
 
         //finally put input to container
         $(input).appendTo(inputContainer);
-        input.addEventListener('keydown', enforceFormat);
-        input.addEventListener('keyup', formatToPhone);
+        $(input).keypress(function (e) {
+            blockNonDigitChars();
+        });
     }
 }
 
@@ -187,55 +188,16 @@ function createCountryListItems(countryList) {
     });
 }
 
-const isNumericInput = (event) => {
-    const key = event.keyCode;
-    return ((key >= 48 && key <= 57) || // Allow number line
-        (key >= 96 && key <= 105) // Allow number pad
-    );
-};
+function blockNonDigitChars() {
+    let regex = new RegExp("^[0-9]+$");
+    var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
 
-const isModifierKey = (event) => {
-    const key = event.keyCode;
-    return (event.shiftKey === true || key === 35 || key === 36) || // Allow Shift, Home, End
-        (key === 8 || key === 9 || key === 13 || key === 46) || // Allow Backspace, Tab, Enter, Delete
-        (key > 36 && key < 41) || // Allow left, up, right, down
-        (
-            // Allow Ctrl/Command + A,C,V,X,Z
-            (event.ctrlKey === true || event.metaKey === true) &&
-            (key === 65 || key === 67 || key === 86 || key === 88 || key === 90)
-        )
-};
-
-// input must be a valid number format or a modifier key, and not longer than ten digits
-const enforceFormat = (event) => {
-    const input = event.target.value.replace(/\D/g, '').substring(0, 10); // First ten digits of input only
-    if (input.length === 10 && !isModifierKey(event)) {
+    if (!regex.test(key) && event.keyCode !== 13) {
         event.preventDefault();
+        return false;
     }
-    if (!isNumericInput(event) && !isModifierKey(event)) {
-        event.preventDefault();
-    }
-};
-// for generate formatted input value
-const formatToPhone = (event) => {
-    if (isModifierKey(event)) {
-        return;
-    }
+}
 
-    const target = event.target;
-    const input = event.target.value.replace(/\D/g, '').substring(0, 10); // First ten digits of input only
-    const zip = input.substring(0, 3);
-    const middle = input.substring(3, 6);
-    const last = input.substring(6, 10);
-
-    if (input.length > 6) {
-        target.value = `(${zip}) ${middle} - ${last}`;
-    } else if (input.length > 3) {
-        target.value = `(${zip}) ${middle}`;
-    } else if (input.length > 0) {
-        target.value = `(${zip}`;
-    }
-};
 
 // search inside country list
 function searchItemOnCountryList(elem) {
